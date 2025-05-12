084f9ddb7ab7ffc40f3de195f01e6980bb23c167
Tonyx27 <tony844151@gmail.com>
2025-05-12 11:36:28 +0530

    1st problem in python

---
+++ module.py
@@ -0,0 +1,32 @@
+print('''
+      Hard work is worthless for those that don’t believe in themselves. – Naruto Uzumaki
+      
+      Somebody told me I’m a failure, I’ll prove them wrong. – Naruto
+
+      Becoming The Hokage Doesn't Mean People Will Acknowledge You. 
+      But When The People Acknowledge You, 
+      You Can Become Hokage. Never Forget Your Friends!
+      - Itachi Uchiha
+
+      Look at me! There’s nothing in my heart, 
+      I don’t feel pain any longer. 
+      There’s no need to feel guilty, Kakashi. 
+      This hole was opened up by this world of hell… Kakashi, it’s okay now. 
+      You don’t need to suffer anymore… Wish for whatever you want. 
+      Anything is possible in this genjutsu world. 
+      That hole in your heart can be filled immediately.
+      - Obito Uchiha
+
+      YOU AND I
+      SIMPLE ARRIVED ON DIFFERENT CONCLUSION
+      LET ME CLARIFY THIS BEFORE WE FIGHT
+      I WILL TEACH YOU EXACTLY
+      WHAT BEING HOKAGE 
+      MEANS TO ME NOW
+      -Sasuke Uchiha
+
+      THE AGE OF SHINOBI IS OVER - kawaki
+      EVEN SO I'M STILL A SHINOBI - Boruto
+
+
+      ''')
